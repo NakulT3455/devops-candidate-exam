1a114ab7c4425430bd6deb184b09b8535968f1fc
NakulT3455 <125851295+NakulT3455@users.noreply.github.com>
2023-08-23 11:50:15 +0530

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -11,21 +11,21 @@ def lambda_handler(event, context):
   payload = event
   request_body = {
         "name": "Nakul Thorat",
-        "subnet": os.environ['subnetId']
-        "emailID": "thoratnakul@gmail.com",
+        "subnet": os.environ['subnetId'],
+        "emailID": "thoratnakul@gmail.com"
         
     }
  
   request_headers = {
-        "X-Siemens-Auth": "test",
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
+        "X-Siemens-Auth": "test"
     }
 
   response = requests.post('https://ij92qpvpma.execute-api.eu-west-1.amazonaws.com/candidate-email_serverless_lambda_stage/data',
                              headers=request_headers,
                              data=json.dumps(request_body))
 
+  
   print(response)
-
 
 
